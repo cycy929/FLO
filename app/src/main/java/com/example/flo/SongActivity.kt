f264cf472f6531c50c75c7fcfc8d992e8163aaf5
Lee Chaeyoung <0929alice@naver.com>
2024-05-01 03:09:50 +0900

    4주차 완료(seekbar,timer,splash)

--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -6,16 +6,19 @@ import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySongBinding
+import java.util.Timer
 
 class SongActivity : AppCompatActivity() {
 
     lateinit var binding : ActivitySongBinding
     lateinit var song: Song
+    lateinit var timer: Timer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
@@ -30,11 +33,11 @@ class SongActivity : AppCompatActivity() {
         }
 
         binding.songMiniplayerIv.setOnClickListener {
-            setPlayerStatus(false)
+            setPlayerStatus(true)
         }
 
         binding.songPauseIv.setOnClickListener {
-            setPlayerStatus(true)
+            setPlayerStatus(false)
         }
 
         binding.songRepeatIv.setOnClickListener {
@@ -52,6 +55,11 @@ class SongActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.interrupt()
+    }
+
     private fun initSong(){
         if(intent.hasExtra("title") && intent.hasExtra("singer")){
             song = Song(
@@ -62,7 +70,7 @@ class SongActivity : AppCompatActivity() {
                 intent.getBooleanExtra("isPlaying",false)
             )
         }
-
+        startTimer()
     }
 
     private fun setPlayer(song: Song){
@@ -77,14 +85,62 @@ class SongActivity : AppCompatActivity() {
     }
 
     fun setPlayerStatus(isPlaying : Boolean){
+        song.isPlaying = isPlaying
+        timer.isPlaying = isPlaying
+
         if(isPlaying){
-            binding.songMiniplayerIv.visibility = View.VISIBLE
-            binding.songPauseIv.visibility = View.GONE
-        }
-        else{
             binding.songMiniplayerIv.visibility = View.GONE
             binding.songPauseIv.visibility = View.VISIBLE
         }
+        else{
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+
+        }
     }
 
+    private fun startTimer(){
+        timer = Timer(song.playTime,song.isPlaying)
+        timer.start()
+    }
+    inner class Timer(private val playTime: Int,var isPlaying: Boolean = true):Thread(){
+
+        private var second : Int = 0
+        private var mills: Float = 0f
+
+        override fun run() {
+            super.run()
+            try {
+                while (true){
+
+                    if (second >= playTime){
+                        break
+                    }
+
+                    if (isPlaying){
+                        sleep(50)
+                        mills += 50
+
+                        runOnUiThread {
+                            binding.songProgressSb.progress = ((mills / playTime)*100).toInt()
+                        }
+
+                        if (mills % 1000 == 0f){
+                            runOnUiThread {
+                                binding.songStartTimeTv.text = String.format("%02d:%02d",second / 60, second % 60)
+                            }
+                            second++
+                        }
+
+                    }
+
+                }
+
+            }catch (e: InterruptedException){
+                Log.d("Song","쓰레드가 죽었습니다. ${e.message}")
+            }
+
+
+        }
+    }
 }
