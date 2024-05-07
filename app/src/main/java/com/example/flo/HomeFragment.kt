e15b99ba749c7eb2cbc3974c2a9b6c619950b784
Lee Chaeyoung <0929alice@naver.com>
2024-05-08 02:42:35 +0900

    6주차 강의 완료(리사이클러뷰 구현, 클릭시 구현)

--- app/src/main/java/com/example/flo/HomeFragment.kt
+++ app/src/main/java/com/example/flo/HomeFragment.kt
@@ -11,14 +11,17 @@ import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
+import com.google.gson.Gson
 import java.util.Timer
 import kotlin.concurrent.scheduleAtFixedRate
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+    private var albumDatas = ArrayList<Album>()
     private val timer = Timer()
     private val handler = Handler(Looper.getMainLooper())
 
@@ -27,8 +30,41 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        FragmentHomeBinding.inflate(inflater, container, false).also { binding = it }
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+//        binding.homeAlbumImgIv1.setOnClickListener {
+//            (context as MainActivity).supportFragmentManager.beginTransaction()
+//                .replace(R.id.main_frm , AlbumFragment())
+//                .commitAllowingStateLoss()
+//        }
+
+        // 데이터 리스트 생성 더미 데이터
+        albumDatas.apply {
+            add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+        }
+
+        // 더미데이터랑 Adapter 연결
+        val albumRVAdapter = AlbumRVAdapter(albumDatas)
+        // 리사이클러뷰에 어댑터를 연결
+        binding.homeTodayMusicAlbumRv.adapter = albumRVAdapter
+        // 레이아웃 매니저 설정
+        binding.homeTodayMusicAlbumRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        albumRVAdapter.setMyItemClickListener(object : AlbumRVAdapter.MyItemClickListener{
+
+            override fun onItemClick(album: Album) {
+                changeAlbumFragment(album)
+            }
+
+            override fun onRemoveAlbum(position: Int) {
+                albumRVAdapter.removeItem(position)
+            }
+        })
 
         binding.homePannelBtnMemoIv.setOnClickListener {
             val intent = Intent(requireActivity(), MemoActivity::class.java)
@@ -61,12 +97,6 @@ class HomeFragment : Fragment() {
                 startActivity(intent)
             }
         }
-            binding.homeTodayAlbumIv.setOnClickListener {
-                (context as MainActivity).supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_frm, AlbumFragment())
-                    .commitAllowingStateLoss()
-
-            }
 
             val bannerAdapter = BannerVPAdapter(this)
             bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
@@ -87,7 +117,20 @@ class HomeFragment : Fragment() {
             startAutoSlide(pannelAdapter)
             return binding.root
         }
-        private fun startAutoSlide(adpater: PannelVPAdapter) {
+
+    private fun changeAlbumFragment(album: Album) {
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, AlbumFragment().apply {
+                arguments = Bundle().apply {
+                    val gson = Gson()
+                    val albumJson = gson.toJson(album)
+                    putString("album", albumJson)
+                }
+            })
+            .commitAllowingStateLoss()
+    }
+
+    private fun startAutoSlide(adpater: PannelVPAdapter) {
             // 일정 간격으로 슬라이드 변경 (3초마다)
             timer.scheduleAtFixedRate(3000, 3000) {
                 handler.post {
